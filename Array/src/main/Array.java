81a8b9ff4b79ddc3559ef4f0444cf4864dc15ff9
Edoardo <edoardoderosaa@gmail.com>
2024-11-29 17:12:31 +0100

    Primo commit

---
+++ Array/src/main/Array.java
@@ -0,0 +1,31 @@
+package main;
+
+import java.util.Scanner;
+
+public class Array {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+		System.out.print("Inserisci il numero da cercare all'interno dell'array: ");
+		int numero = scanner.nextInt();
+
+		boolean caso = false;
+		int length = array.length;
+		for (int i = 0; i < length; i++) {
+			if (numero == array[i]) {
+				System.out.print(i);
+				caso = true;
+			}
+
+		}
+		if (!caso) {
+			System.out.print("Non trovato");
+		}
+		scanner.close();
+
+	}
+
+}
